710e8ba11c86aa31931500e79088576a914e5f4c
Playstation-oficial <overigor313@gmail.com>
2025-09-18 00:07:17 +0000

    desculpa por ter esquecido conteúdo antes, eu sou muito lerdo. eu adicionei a segunda cidade e a divisão para saber a densidade populacional <3

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -16,7 +16,7 @@ int main(){
     scanf("%s", codigo);
 
     printf("digite o nome: ");
-    scanf(" %[^\n]", nome);
+    scanf(" %s", nome);
 
     printf("digite a população: ");
     scanf("%d", &populaçao);
@@ -34,10 +34,52 @@ int main(){
     printf("Estado: %s\n", estado);
     printf("Codigo: %s\n", codigo);
     printf("Nome: %s\n", nome);
-    printf("Populacao: %d\n", populacao);
+    printf("Populacao: %d\n", populaçao);
     printf("Area: %.2f km²\n", area);
     printf("PIB: %.2f\n", PIB);
     printf("Turismo: %d pontos\n", turismo);
+
+    printf("----------\n\n");
+
+    ////////////////////////////////////////////////////////////////
+
+     char estado2[20];
+    char codigo2[5];
+    char nome2[50];
+    int populaçao2;
+    float area2;
+    float PIB2;
+    int turismo2;
+
+    printf("digite seu estado: ");
+    scanf("%s", estado2);
+
+    printf("digite o codigo: ");
+    scanf("%s", codigo2);
+
+    printf("digite o nome: ");
+    scanf(" %s", nome2);
+
+    printf("digite a população: ");
+    scanf("%d", &populaçao2);
+
+    printf("digite a área: ");
+    scanf("%f", &area2);
+
+    printf("digite o PIB: ");
+    scanf("%f", &PIB2);
+
+    printf("digite o turismo: ");
+    scanf("%d", &turismo2);
+
+    printf("\n--- SUA SEGUNDA CIDADE ---\n");
+    printf("Estado: %s\n", estado2);
+    printf("Codigo: %s\n", codigo2);
+    printf("Nome: %s\n", nome2);
+    printf("Populacao: %d\n", populaçao2);
+    printf("Area: %.2f km²\n", area2);
+    printf("PIB: %.2f\n", PIB2);
+    printf("Turismo: %d pontos\n", turismo2);
 
 return 0; 
 }
