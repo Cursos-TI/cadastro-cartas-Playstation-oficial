aafc4a63c1c8da10c6bb739907c2421afc1f08e4
Playstation-oficial <overigor313@gmail.com>
2025-09-25 23:12:55 +0000

    espero ter feito certo, eu mudei o método de comparação das cartas, agora é usando o IF

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main(){
-    char estado[20];
+    char estado[20]; //// variáveis 
     char codigo[5];
     char nome[50];
     signed long int populaçao;
@@ -14,7 +14,7 @@ int main(){
 
     float divisaoPIB;
 
-    printf("digite seu estado: ");
+    printf("digite seu estado: ");  //// scanf para digitarem
     scanf("%s", estado);
 
     printf("digite o codigo: ");
@@ -35,13 +35,13 @@ int main(){
     printf("digite o turismo: ");
     scanf("%d", &turismo);
     
-    divisao = populaçao / area; 
+    divisao = populaçao / area;  /// somando os resultados para densidade e PIB per capita
 
     divisaoPIB = PIB / (float)populaçao;
 
     SuperPoder = (float)populaçao + area + PIB + (float)turismo + divisao + divisaoPIB;
 
-    printf("\n--- SUA CIDADE ---\n");
+    printf("\n--- SUA CIDADE ---\n");   /// apresentação dos resultados
     printf("Estado: %s\n", estado);
     printf("Codigo: %s\n", codigo);
     printf("Nome: %s\n", nome);
@@ -54,11 +54,7 @@ int main(){
 
     printf("----------\n\n");
 
-    ////////////////////////////////////////////////////////////////
-//eu tinha esquecido que eram duas cartas, perdão!
-
-// adicionei a segunda e um pulo entre as linhas <3
-     char estado2[20];
+     char estado2[20];   //// segundas variáveis
     char codigo2[5];
     char nome2[50];
     signed long int populaçao2;
@@ -72,7 +68,7 @@ int main(){
 
     float SuperPoder2;
 
-    printf("digite seu estado: ");
+    printf("digite seu estado: "); ///// segundo scanf para receber as informações
     scanf("%s", estado2);
 
     printf("digite o codigo: ");
@@ -93,13 +89,13 @@ int main(){
     printf("digite o turismo: ");
     scanf("%d", &turismo2);
 
-    divisao2 = populaçao2 / area2; 
+    divisao2 = populaçao2 / area2;  /// somando para ter densidade e PIB per capita
 
     divisaoPIB2 = PIB2 / (float)populaçao2;
 
     SuperPoder2 = (float)populaçao2 + area2 + PIB2 + (float)turismo2 + divisao2 + divisaoPIB2;
 
-    printf("\n--- SUA SEGUNDA CIDADE ---\n");
+    printf("\n--- SUA SEGUNDA CIDADE ---\n");  /// segunda apresentação dos resultados
     printf("Estado: %s\n", estado2);
     printf("Codigo: %s\n", codigo2);
     printf("Nome: %s\n", nome2);
@@ -112,14 +108,41 @@ int main(){
 
     printf("----------\n\n");
        
-    printf("\n --- COMPARAÇÃO ENTRE AS CARTAS --- \n\n");
-    printf("População: %d\n", populaçao > populaçao2);
-    printf("Área: %d\n", area > area2);
-    printf("PIB: %d\n", PIB > PIB2);
-    printf("Turismo: %d\n", turismo > turismo2);
-    printf("Densidade Populacional: %d\n", divisao < divisao2);
-    printf("PIB per Capita: %d\n", divisaoPIB > divisaoPIB2);
-    printf("Super Poder: %d\n", SuperPoder > SuperPoder2);
+    printf("\n --- COMPARAÇÃO ENTRE AS CARTAS --- \n\n"); /// a comparação e vencendor
+   if(populaçao > populaçao2){
+    printf("População: Carta1 ganhou! \n");
+   }else{
+    printf("População: Carta2 ganhou!\n");
+   }
 
+   if(area > area2){
+    printf("Área: Carta1 ganhou! \n");
+   }else{
+    printf("Área: Carta2 ganhou!\n");
+   }
+
+    if(PIB > PIB2){
+    printf("PIB: Carta1 ganhou! \n");
+   }else{
+    printf("PIB: Carta2 ganhou!\n");
+   }
+
+    if(turismo > turismo2){
+    printf("Turismo: Carta1 ganhou! \n");
+   }else{
+    printf("Turismo: Carta2 ganhou!\n");
+   }
+
+    if(divisao < divisao2){
+    printf("Densidade Populacional: Carta1 ganhou! \n");
+   }else{
+    printf("Densidade Populacional: Carta2 ganhou!\n");
+   }
+
+    if(divisaoPIB > divisaoPIB2){
+    printf("PIB per Capita: Carta1 ganhou! \n");
+   }else{
+    printf("PIB per Capita: Carta2 ganhou!\n");
+   }
 return 0;
 }
